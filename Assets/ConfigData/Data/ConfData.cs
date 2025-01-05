0dd8dd383cb109e965bf2d2c20a5334bede97319
zxjun2002 <zxjun200222@163.com>
2025-01-05 22:40:43 +0800

    feat:新增配置表格式结构，后面喂给AI

--- Assets/ConfigData/Data/ConfData.cs
+++ Assets/ConfigData/Data/ConfData.cs
@@ -1,12 +1,18 @@
 using MemoryPack;
 
 [MemoryPackable]
-public partial class ConfData{
-    
+public partial class ConfData
+{
+    public Item[] ItemCt;
 }
 
 [MemoryPackable]
 public partial class Item
 {
-    
+    public int ItemId;
+    public string ItemName;
+    public long ItemPId;
+    public string ItemJson;
+    public int[] ItemList;
+    public string[] ItemDesList;
 }
