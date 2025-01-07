f57fcc1bdb741bd93ca3ffa108fb9a30917116ad
zhangxujun <zhangxujun@xmfunny.com>
2025-01-07 09:56:04 +0800

    feat:移动配置表位置

--- Assets/ConfigTable/ResEditorConfig.cs
+++ Assets/ConfigTable/ResEditorConfig.cs
@@ -1,6 +1,6 @@
 public class ResEditorConfig 
 {
-    public const string CSV_Path = "/Config";
+    public const string CSV_Path = "/../Config";//..代表上一级目录
     public const string ConfData_Path = "/ConfigData/Data/ConfData.cs";
     public const string ConfsAsset_Path = "Assets/ConfigData/Configs/ConfData.byte";
 }
