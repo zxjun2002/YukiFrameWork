c31db5ceeed54ea8b10ebe9e5f1d5f3d1529d759
zhangxujun <zhangxujun@xmfunny.com>
2025-01-20 21:19:13 +0800

    fix:动态设置看板值方法

--- Assets/Plugins/NodeBasedDialogueSystem/com.subtegral.dialoguesystem/Runtime/DialogueContainer.cs
+++ Assets/Plugins/NodeBasedDialogueSystem/com.subtegral.dialoguesystem/Runtime/DialogueContainer.cs
@@ -11,5 +11,18 @@ namespace Subtegral.DialogueSystem.DataContainers
         public List<DialogueNodeData> DialogueNodeData = new List<DialogueNodeData>();
         public List<ExposedProperty> ExposedProperties = new List<ExposedProperty>();
         public List<CommentBlockData> CommentBlockData = new List<CommentBlockData>();
+
+        public void SetValue(string key, string value)
+        {
+            ExposedProperty property = ExposedProperties.Find(x => x.PropertyName == key);
+            if (property == null)
+            {
+                property = ExposedProperty.CreateInstance();
+                property.PropertyName = key;
+                ExposedProperties.Add(property);
+            }
+
+            property.PropertyValue = value;
+        }
     }
 }
