d6f52a36cb13427134f02bc9ffc0e4051d156bdd
zhangxujun <zhangxujun@xmfunny.com>
2025-07-08 10:37:20 +0800

    fix:配置表懒加载方案

--- Assets/Runtime/ConfigTable/ConfigTable.cs
+++ Assets/Runtime/ConfigTable/ConfigTable.cs
@@ -6,23 +6,26 @@ using MIKUFramework.IOC;
 public class ConfigTable: IConfigTable
 {
     private ConfData confs;
-    private Dictionary<Type, IRacastSet> racastSets;
+    private Dictionary<Type, Lazy<IRacastSet>> racastSets;
 
     public void Init(string url)
     {
         confs = FileBytesUtil.LoadConfFromBinary<ConfData>(ResEditorConfig.ConfsAsset_Path);
-        racastSets = new Dictionary<Type, IRacastSet>();
         //TODO：新增配置表需加入racastSets中
-        racastSets.Add(typeof(ItemRacastSet), new ItemRacastSet(confs));
-        racastSets.Add(typeof(BuffCtRacastSet), new BuffCtRacastSet(confs));
-        racastSets.Add(typeof(EffectCtRacastSet), new EffectCtRacastSet(confs));
-        
+        racastSets = new Dictionary<Type, Lazy<IRacastSet>>
+        {
+            { typeof(ItemRacastSet),     new Lazy<IRacastSet>(() => new ItemRacastSet(confs)) },
+            { typeof(BuffCtRacastSet),   new Lazy<IRacastSet>(() => new BuffCtRacastSet(confs)) },
+            { typeof(EffectCtRacastSet), new Lazy<IRacastSet>(() => new EffectCtRacastSet(confs)) },
+        };        
         GameLogger.Log("配置表初始化成功！！！");
     }
 
     public T GetConfig<T>() where T : struct, IRacastSet
     {
-        racastSets.TryGetValue(typeof(T), out var config);
-        return (T)config; // 进行类型转换
+        var type = typeof(T);
+        if (!racastSets.TryGetValue(type, out var lazy))
+            throw new KeyNotFoundException($"未注册配置类型：{type.Name}");
+        return (T)lazy.Value; // 第一次访问时才 new，以后直接复用
     }
 }
